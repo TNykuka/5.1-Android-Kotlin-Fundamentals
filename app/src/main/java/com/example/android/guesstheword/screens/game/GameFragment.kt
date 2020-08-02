1913c2ddd6c52fe533caea33d117951a29d3a10b
TNyKk <kingkuka088@gmail.com>
2020-08-02 16:15:46 +0700

    Android Kotlin Fundamentals 05.1-6 step 1: Move data fields and data processing to the ViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -33,24 +33,15 @@ import timber.log.Timber
  */
 class GameFragment : Fragment() {
 
-    private lateinit var viewModel: GameViewModel
-
-    // The current word
-    private var word = ""
-
-    // The current score
-    private var score = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
 
     private lateinit var binding: GameFragmentBinding
 
+
+    private lateinit var viewModel: GameViewModel
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        Timber.i("Called ViewModelProviders.of")
-        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
@@ -60,8 +51,8 @@ class GameFragment : Fragment() {
                 false
         )
 
-        resetList()
-        nextWord()
+        Timber.i("Called ViewModelProviders.of")
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
@@ -71,37 +62,8 @@ class GameFragment : Fragment() {
 
     }
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-                "queen",
-                "hospital",
-                "basketball",
-                "cat",
-                "change",
-                "snail",
-                "soup",
-                "calendar",
-                "sad",
-                "desk",
-                "guitar",
-                "home",
-                "railway",
-                "zebra",
-                "jelly",
-                "car",
-                "crow",
-                "trade",
-                "bag",
-                "roll",
-                "bubble"
-        )
-        wordList.shuffle()
-    }
 
-    /** Methods for buttons presses **/
+    /** Methods for button click handlers **/
 
     private fun onSkip() {
         score--
@@ -111,18 +73,6 @@ class GameFragment : Fragment() {
     private fun onCorrect() {
         score++
         nextWord()
-    }
-
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
-            word = wordList.removeAt(0)
-        }
-        updateWordText()
-        updateScoreText()
     }
 
 
